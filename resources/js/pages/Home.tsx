b55d257b58e1b587de99b43d59ad9816e2a23012
CousemaAnjary <anjaryabdillah@gmail.com>
2024-09-15 20:37:37 +0300

    Add @radix-ui/react-avatar package

--- resources/js/pages/Home.tsx
+++ resources/js/pages/Home.tsx
@@ -1,20 +1,27 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
     /**
      * ! STATE (état, données) de l'application
      */
-    
-    
+
+
     /**
      * ! COMPORTEMENT (méthodes, fonctions) de l'application
      */
-    
-    
+
+
     /**
      * ! AFFICHAGE (render) de l'application
      */
     return (
         <>
-            <h1>Accueil</h1>
+            <h1 className="text-3xl font-bold underline">
+                Hello world!
+            </h1>
+            <Button>
+                Cliquez-moi
+            </Button>
         </>
     )
 }
