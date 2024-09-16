7eea916749313ad6a1feaa6769b4d2fe5b93c702
CousemaAnjary <anjaryabdillah@gmail.com>
2024-09-16 21:36:38 +0300

    Refactorisation du contenu et des styles de l'accordéon dans le composant AboutSection

--- resources/js/components/AboutSection.tsx
+++ resources/js/components/AboutSection.tsx
@@ -1,15 +1,22 @@
+import { useState } from 'react'
 import about from '../assets/images/about.png'
+import { FiMinus, FiPlus } from 'react-icons/fi'
 import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from './ui/accordion'
+
 
 export default function AboutSection() {
     /**
      * ! STATE (état, données) de l'application
      */
+    const [openItem, setOpenItem] = useState<string | null>(null)
 
 
     /**
      * ! COMPORTEMENT (méthodes, fonctions) de l'application
      */
+    const handleToggle = (item: string): void => {
+        setOpenItem(openItem === item ? null : item)
+    }
 
 
     /**
@@ -20,7 +27,7 @@ export default function AboutSection() {
             <div className=" relative bg-slate-50 px-40 py-20 flex ">
                 {/* Section Texte */}
                 <div className="md:w-1/2">
-                    <h3 className="text-blue-600 font-mono font-semibold text-xl uppercase mb-2">À propos de nous</h3>
+                    <h3 className="text-blue-900 font-mono font-semibold text-xl uppercase mb-2">À propos de nous</h3>
                     <h2 className="text-4xl font-bold mb-4">
                         MIA, votre partenaire digital pour une comptabilité simplifiée
                     </h2>
@@ -31,19 +38,34 @@ export default function AboutSection() {
 
                     <Accordion type="single" collapsible className="w-full">
                         <AccordionItem value="item-1" className='mb-3'>
-                            <AccordionTrigger className="bg-white  p-4 transition-colors data-[state=open]:bg-blue-500 data-[state=open]:text-white decoration-transparent">Quels services comptables proposez-vous ?</AccordionTrigger>
+                            <AccordionTrigger className="bg-white  p-4 transition-colors data-[state=open]:bg-blue-900 data-[state=open]:text-white decoration-transparent" onClick={() => handleToggle('item-1')}>
+                                Quels services comptables proposez-vous ?
+                                <span className="ml-2">
+                                    {openItem === 'item-1' ? <FiMinus /> : <FiPlus />}
+                                </span>
+                            </AccordionTrigger>
                             <AccordionContent className='p-5'>
                                 Nous offrons des services complets : récupération et traitement des pièces comptables, révision des comptes, lettrage, rapprochement bancaire et établissement du bilan annuel.
                             </AccordionContent>
                         </AccordionItem>
                         <AccordionItem value="item-2" className='mb-3'>
-                            <AccordionTrigger className="bg-white p-4 transition-colors data-[state=open]:bg-blue-500 data-[state=open]:text-white decoration-transparent">Quelles solutions fiscales mettez-vous en place ?</AccordionTrigger>
+                            <AccordionTrigger className="bg-white p-4 transition-colors data-[state=open]:bg-blue-900 data-[state=open]:text-white decoration-transparent" onClick={() => handleToggle('item-2')}>
+                                Quelles solutions fiscales mettez-vous en place ?
+                                <span className="ml-2">
+                                    {openItem === 'item-2' ? <FiMinus /> : <FiPlus />}
+                                </span>
+                            </AccordionTrigger>
                             <AccordionContent className='p-5'>
                                 Nous nous occupons de la préparation et de la télédéclaration de la TVA, de la liasse fiscale, des annexes et plaquettes, ainsi que de l'envoi des déclarations fiscales (DAS2).
                             </AccordionContent>
                         </AccordionItem>
                         <AccordionItem value="item-3" className='mb-3'>
-                            <AccordionTrigger className="bg-white p-4 transition-colors data-[state=open]:bg-blue-500 data-[state=open]:text-white decoration-transparent">Quels sont vos services administratifs ?</AccordionTrigger>
+                            <AccordionTrigger className="bg-white p-4 transition-colors data-[state=open]:bg-blue-900 data-[state=open]:text-white decoration-transparent" onClick={() => handleToggle('item-3')}>
+                                Quels sont vos services administratifs ?
+                                <span className="ml-2">
+                                    {openItem === 'item-3' ? <FiMinus /> : <FiPlus />}
+                                </span>
+                            </AccordionTrigger>
                             <AccordionContent className='p-5'>
                                 Nous proposons des services de saisie de données, de service après-vente, de relance clients, d'assistanat virtuel et de facturation clients, ainsi que d'autres tâches administratives sur demande.
                             </AccordionContent>
