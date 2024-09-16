dcf5d9cc28f1206e0e39755814cb2153c7ab164d
CousemaAnjary <anjaryabdillah@gmail.com>
2024-09-16 14:57:05 +0300

    Update HeroSection component layout and content

--- resources/js/components/HeroSection.tsx
+++ resources/js/components/HeroSection.tsx
@@ -1,3 +1,5 @@
+import { Button } from "./ui/button";
+
 export default function HeroSection() {
     /**
      * ! STATE (état, données) de l'application
@@ -16,19 +18,20 @@ export default function HeroSection() {
         <>
             <div className="relative  flex flex-col mt-32 text-white p-4 mx-32">
                 <h1 className="text-6xl font-bold">
-                    Simplifiez Votre Comptabilité <br /> et Développez Votre Entreprise.
+                    Simplifiez <br /> Votre Comptabilité <br /> et Développez Votre Entreprise.
                 </h1>
-                <p className="text-lg mt-4 max-w-2xl">
-                    Plus de 35 ans d'expertise pour simplifier la gestion de votre entreprise. <br />
-                    Gagnez du temps, concentrez-vous sur l'essentiel : vos clients.
+                <p className="text-xl font-normal mt-6">
+                    "Nous déléguer une partie de vos activités,<br />
+                    c'est gagner du temps pour mieux accompagner vos précieux clients."
                 </p>
                 <div className="mt-8 space-x-4">
-                    <button className="bg-purple-600 hover:bg-purple-700 text-white py-2 px-6 rounded-full">
+                    <Button variant={"default"} size={"lg"} className="bg-blue-800 hover:bg-blue-900 py-6 px-6">
                         Notre Solution
-                    </button>
-                    <button className="bg-transparent border border-white hover:bg-white hover:text-black text-white py-2 px-6 rounded-full">
+                    </Button>
+
+                    <Button variant={"ghost"} size={"lg"} className="border border-white  py-6 px-6" >
                         En Savoir Plus
-                    </button>
+                    </Button>
                 </div>
             </div>
         </>
