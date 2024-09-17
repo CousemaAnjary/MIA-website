09559518eff9c7412534bc87e0777e4268888fe8
CousemaAnjary <anjaryabdillah@gmail.com>
2024-09-18 00:00:31 +0300

    Refactoriser les styles de la section OffreSection et mettre à jour les offres

--- resources/js/components/OffreSection.tsx
+++ resources/js/components/OffreSection.tsx
@@ -1,4 +1,4 @@
-import { Check, Star } from "lucide-react"
+import { Check, Star } from "lucide-react";
 
 export default function Component() {
     const tiers = [
@@ -46,7 +46,7 @@ export default function Component() {
             ],
             featured: false,
         },
-    ]
+    ];
 
     return (
         <div className="bg-neutral-50">
@@ -56,81 +56,44 @@ export default function Component() {
                         <span className="inline-block w-10 h-0.5 bg-white mr-2"></span> {/* Ligne à gauche */}
                         Nos offres
                     </h3>
-                    <p className="mx-auto mt-4 text-xl ">Get started on our free plan and upgrade when you are
-                        ready.</p>
+                    <p className="mx-auto mt-4 text-xl">Découvrez nos différentes offres adaptées à vos besoins.</p>
                 </div>
-                <div className="mt-24 mx-32 space-y-12 lg:space-y-0 lg:grid lg:grid-cols-2 lg:gap-x-8 ">
-                    <div className="relative p-8  border border-gray-200 rounded-2xl shadow-sm flex flex-col">
-                        <div className="flex-1">
-                            <h3 className="text-xl font-semibold ">Free</h3>
-                            <p className="mt-4 flex items-baseline ">
-                                <span className="text-5xl font-extrabold tracking-tight">$0</span><span className="ml-1 text-xl font-semibold">/month</span>
-                            </p>
-                            <p className="mt-6 ">You just want to discover</p>
-                            <ul role="list" className="mt-6 space-y-6">
-                                <li className="flex"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
-                                    fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
-                                    stroke-linejoin="round" className="flex-shrink-0 w-6 h-6 text-emerald-500" aria-hidden="true">
-                                    <polyline points="20 6 9 17 4 12"></polyline>
-                                </svg><span className="ml-3 ">10 Credits</span></li>
-                                <li className="flex"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
-                                    fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
-                                    stroke-linejoin="round" className="flex-shrink-0 w-6 h-6 text-emerald-500" aria-hidden="true">
-                                    <polyline points="20 6 9 17 4 12"></polyline>
-                                </svg><span className="ml-3 ">Generate video (2 credits)</span></li>
-                                <li className="flex"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
-                                    fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
-                                    stroke-linejoin="round" className="flex-shrink-0 w-6 h-6 text-emerald-500" aria-hidden="true">
-                                    <polyline points="20 6 9 17 4 12"></polyline>
-                                </svg><span className="ml-3 ">Quizz (1 credits) </span></li>
-                            </ul>
-                        </div><a
-                            className="bg-emerald-50 text-emerald-700 hover:bg-emerald-100 mt-8 block w-full py-3 px-6 border border-transparent rounded-md text-center font-medium"
-                            href="/auth/login">Signup for free</a>
-                    </div>
-                    <div className="relative p-8  border border-gray-200 rounded-2xl shadow-sm flex flex-col">
-                        <div className="flex-1">
-                            <h3 className="text-xl font-semibold ">Pro</h3>
-                            <p
-                                className="absolute top-0 py-1.5 px-4 bg-emerald-500 text-white rounded-full text-xs font-semibold uppercase tracking-wide  transform -translate-y-1/2">
-                                Most popular</p>
-                            <p className="mt-4 flex items-baseline ">
-                                <span className="text-5xl font-extrabold tracking-tight">$12</span><span className="ml-1 text-xl font-semibold">/month</span>
-                            </p>
-                            <p className="mt-6 ">You want to learn and have a personal assistant</p>
-                            <ul role="list" className="mt-6 space-y-6">
-                                <li className="flex"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
-                                    fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
-                                    stroke-linejoin="round" className="flex-shrink-0 w-6 h-6 text-emerald-500" aria-hidden="true">
-                                    <polyline points="20 6 9 17 4 12"></polyline>
-                                </svg><span className="ml-3 ">30 credits</span></li>
-                                <li className="flex"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
-                                    fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
-                                    stroke-linejoin="round" className="flex-shrink-0 w-6 h-6 text-emerald-500" aria-hidden="true">
-                                    <polyline points="20 6 9 17 4 12"></polyline>
-                                </svg><span className="ml-3 ">Powered by GPT-4 (more accurate)</span></li>
-                                <li className="flex"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
-                                    fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
-                                    stroke-linejoin="round" className="flex-shrink-0 w-6 h-6 text-emerald-500" aria-hidden="true">
-                                    <polyline points="20 6 9 17 4 12"></polyline>
-                                </svg><span className="ml-3 ">Generate video (2 credits)</span></li>
-                                <li className="flex"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
-                                    fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
-                                    stroke-linejoin="round" className="flex-shrink-0 w-6 h-6 text-emerald-500" aria-hidden="true">
-                                    <polyline points="20 6 9 17 4 12"></polyline>
-                                </svg><span className="ml-3 ">Quizz (1 credits) </span></li>
-                                <li className="flex"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
-                                    fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
-                                    stroke-linejoin="round" className="flex-shrink-0 w-6 h-6 text-emerald-500" aria-hidden="true">
-                                    <polyline points="20 6 9 17 4 12"></polyline>
-                                </svg><span className="ml-3 ">Analytics on the quizz</span></li>
-                            </ul>
-                        </div><a
-                            className="bg-emerald-500 text-white  hover:bg-emerald-600 mt-8 block w-full py-3 px-6 border border-transparent rounded-md text-center font-medium"
-                            href="/auth/login">Signup for free</a>
-                    </div>
+                <div className="mt-24 mx-32 space-y-12 lg:space-y-0 lg:grid lg:grid-cols-2 lg:gap-x-8">
+                    {tiers.map((tier, index) => (
+                        <div
+                            key={index}
+                            className={`relative p-8 border border-gray-200 rounded-2xl shadow-sm flex flex-col ${tier.featured ? "border-2 border-emerald-500" : ""
+                                }`}
+                        >
+                            <div className="flex-1">
+                                <h3 className="text-xl font-semibold">{tier.name}</h3>
+                                {tier.featured && (
+                                    <p className="absolute top-0 py-1.5 px-4 bg-emerald-500 text-white rounded-full text-xs font-semibold uppercase tracking-wide transform -translate-y-1/2">
+                                        Most popular
+                                    </p>
+                                )}
+                                <p className="mt-4 flex items-baseline">
+                                    <span className="text-5xl font-extrabold tracking-tight">{tier.price}</span>
+                                </p>
+                                <ul role="list" className="mt-6 space-y-6">
+                                    {tier.description.map((feature, i) => (
+                                        <li key={i} className="flex">
+                                            <Check className="flex-shrink-0 w-6 h-6 text-emerald-500" />
+                                            <span className="ml-3">{feature}</span>
+                                        </li>
+                                    ))}
+                                </ul>
+                            </div>
+                            <a
+                                className="bg-emerald-50 text-emerald-700 hover:bg-emerald-100 mt-8 block w-full py-3 px-6 border border-transparent rounded-md text-center font-medium"
+                                href="#"
+                            >
+                                Choisir {tier.name}
+                            </a>
+                        </div>
+                    ))}
                 </div>
             </div>
         </div>
-    )
+    );
 }
