8e62cf5dc8c87dc8c2de77c91ba7b3947d3e5ac7
CousemaAnjary <anjaryabdillah@gmail.com>
2024-09-17 22:27:22 +0300

    Mettre à jour les fichiers de contenu dans tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,10 @@
 module.exports = {
   darkMode: ["class"],
   content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
+    "./resources/**/*.blade.php",
+    "./resources/**/*.js",
+	"./resources/**/*.tsx",
+    "./resources/**/*.vue",
   ],
   prefix: "",
   theme: {
