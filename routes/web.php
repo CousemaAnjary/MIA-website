73f8ec04d77761d931f3f069ea44691b8da3ec80
CousemaAnjary <anjaryabdillah@gmail.com>
2024-09-27 22:09:08 +0300

    Refactoriser les routes pour ajouter la fonctionnalité d'envoi d'e-mail depuis le formulaire de contact

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ContactController;
 use App\Http\Controllers\PageController;
 use Illuminate\Support\Facades\Route;
 
@@ -15,3 +16,4 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', [PageController::class, 'home'])->name('home');
+Route::post('/send-email', [ContactController::class, 'sendEmail']);
