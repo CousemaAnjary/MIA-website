a544ef4a95ce48d1160e5ec45f5c591f99c1c273
CousemaAnjary <anjaryabdillah@gmail.com>
2024-10-13 14:16:36 +0300

    Refactoriser la structure du projet pour ajouter une structure de base à la page d'accueil

--- resources/views/pages/home/index.blade.php
+++ resources/views/pages/home/index.blade.php
@@ -7,5 +7,16 @@
 @section('content')
 
 
+    <div class="">
+        {{-- En-tête --}}
+        <header></header>
+
+        {{-- Contenu --}}
+        <main></main>
+
+        {{-- Pied de page --}}
+        <footer></footer>
+    </div>
+
 
 @endsection
