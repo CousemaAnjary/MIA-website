247bc379c2f7e8247c7c2ceac09e3ae5790a604e
CousemaAnjary <anjaryabdillah@gmail.com>
2024-10-13 15:36:55 +0300

    Refactoriser la structure du projet pour ajuster la taille du logo dans la navbar, modifier l'alignement du texte dans la section d'accueil et ajouter des fichiers CSS pour le style de la page d'accueil.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,7 +15,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
 
     <!-- Ton CSS personnalisé si nécessaire -->
-   
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 
 </head>
 
