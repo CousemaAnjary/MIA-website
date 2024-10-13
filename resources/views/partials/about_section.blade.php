822c78c41cbe2eb589cc2a391cbe39df7727fcbe
CousemaAnjary <anjaryabdillah@gmail.com>
2024-10-13 22:25:47 +0300

    Refactoriser la structure du projet pour ajuster l'alignement du texte dans la section d'accueil, modifier les boutons du menu et ajouter une section "Nos missions" dans la page d'accueil

--- resources/views/partials/about_section.blade.php
+++ resources/views/partials/about_section.blade.php
@@ -1,26 +1,39 @@
-<div class="about-section py-5">
+<div class="about-section py-5 text-white" style="background-color: #03224c">
     <div class="container d-flex flex-column flex-md-row align-items-center gap-5">
         <!-- Ajout de gap-3 pour réduire l'espace -->
         <!-- Texte -->
         <div class="col-md-6">
             <h3 class="text-uppercase font-weight-bold mb-4">
-                <span class="border-bottom border-primary pb-1">À propos de nous</span>
+                <span class="border-bottom fw-bold border-white pb-1">À propos de nous</span>
             </h3>
-            <h2 class="font-weight-bold mb-3">
+            {{-- <h2 class="font-weight-bold mb-3">
                 MIA, votre partenaire digital pour une comptabilité simplifiée
-            </h2>
+            </h2> --}}
+            <p class="fst-italic fs-5">
+                Vous méritez la tranquillité sans perdre en qualité. Laissez-nous vous aider et vivez sans pression.
+            </p>
             <p class="text-muted mb-4">
                 MIA, diminutif de Mi-Assiste, est une société basée à Madagascar qui propose des services de
                 sous-traitance pour les PME et cabinets comptables. Avec plus de 10 ans d'expérience, nous sommes le
                 partenaire digital idéal pour accompagner la croissance de votre entreprise en France.
             </p>
-
-            <p class="fst-italic">
-                Vous méritez la tranquillité sans perdre en qualité. Laissez-nous vous aider et vivez sans pression.
+            <p class="text-muted mb-4">
+                Elle est née de la passion de deux personnes qui ont décidé de s’associer après plus de dix années
+                d’expérience en
+                salariat dans le domaine de la comptabilité française. Elle veut apporter une pierre à l’édifice en
+                contribuant à la
+                création d’emploi et réduction du taux de chômage du pays. Elle souhaite solutionner la difficulté à
+                recruter des
+                cabinets comptables en France.
+            </p>
+            <p class="text-muted mb-4">
+                MIA met à votre disposition plusieurs profils de collaborateurs en fonction de vos besoins pour traiter
+                de la meilleure
+                façon possible les tâches qui nous seront confiées.
             </p>
 
             <!-- Accordéon -->
-            <div class="accordion" id="accordionExample">
+            {{-- <div class="accordion" id="accordionExample">
                 <div class="accordion-item">
                     <h2 class="accordion-header" id="headingOne">
                         <button class="accordion-button" type="button" data-bs-toggle="collapse"
@@ -69,12 +82,12 @@
                         </div>
                     </div>
                 </div>
-            </div>
+            </div> --}}
         </div>
 
         <!-- Image -->
         <div class="col-md-6 mt-4 mt-md-0">
-            <img src="{{ asset('images/about.png') }}" alt="À propos de MIA" class="img-fluid rounded">
+            <img src="{{ asset('images/logo.png') }}" alt="À propos de MIA" class="img-fluid rounded">
         </div>
     </div>
 </div>
