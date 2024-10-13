dd2d9b493ffd1e35d1e30100198c3f05ca8b31c6
CousemaAnjary <anjaryabdillah@gmail.com>
2024-10-13 15:00:12 +0300

    Refactoriser la structure du projet pour ajuster la taille du logo dans la navbar et modifier l'alignement du texte dans la section d'accueil

--- resources/views/partials/hero_section.blade.php
+++ resources/views/partials/hero_section.blade.php
@@ -1,13 +1,10 @@
-<div class="d-flex flex-column justify-content-center align-items-center h-100 text-center text-white">
-    <h1 class="display-4 font-weight-bold mb-4">
-        Simplifiez Votre Comptabilité <br> et Développez Votre Entreprise.
+<div class="container d-flex flex-column justify-content-center align-items-start h-100 text-start text-white">
+    <h1 class=" font-weight-bold mb-4">
+        Nous déléguer une partie de vos activités, <br> c'est gagner du temps pour mieux accompagner vos précieux
+        clients
     </h1>
 
-    <p class="lead mb-4">
-        "Nous déléguer une partie de vos activités, <br> c'est gagner du temps pour mieux accompagner vos précieux clients."
-    </p>
-
-    <div class="mt-4 d-flex flex-column flex-md-row justify-content-center gap-3">
+    <div class="mt-4 d-flex flex-column flex-md-row justify-content-start gap-3">
         <a href="#" class="btn btn-primary btn-lg">
             <i class="fas fa-gift"></i> Nos offres
         </a>
