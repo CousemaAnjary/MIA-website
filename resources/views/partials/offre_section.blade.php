75aa29e818df7a5a5c28c1dffe428dd9d59f8fb8
CousemaAnjary <anjaryabdillah@gmail.com>
2024-10-17 21:41:40 +0300

    Refactoriser la section "À propos de MIA" pour ajuster l'affichage du membre du groupe

--- resources/views/partials/offre_section.blade.php
+++ resources/views/partials/offre_section.blade.php
@@ -1,11 +1,12 @@
-<div class="text-white py-5" style="background-color: #03224c" >
+<div class="text-white py-5" style="background-color: #03224c">
     <!-- Titre de la section -->
     <div class="container" data-aos="fade-right">
         <h3 class="text-uppercase  mb-4">
             <span class="border-bottom fw-bold border-white">Nos offres</span>
         </h3>
         <p class="text-white fw-medium fs-5 fst-italic" data-aos="fade-right">
-            " Vous méritez l'efficacité, laissez-nous vous aider avec rigueur, et ensemble, <br> bâtissons l'avenir de votre
+            " Vous méritez l'efficacité, laissez-nous vous aider avec rigueur, et ensemble, <br> bâtissons l'avenir de
+            votre
             grandeur "
         </p>
     </div>
@@ -18,19 +19,19 @@
                 $tiers = [
                     [
                         'name' => 'BASIC',
-                        'price' => '800€',
-                        'title' => '1 collaborateur junior :',
-                        'description' => ['Saisie et traitement', 'Nécessite une révision au bout', 'Français moyen'],
+                        'price' => '850€',
+                        'title' => '1 collaborateur niveau junior :',
+                        'description' => ['Tenue comptable (saisie, révision, lettrage, rapprochement bancaire)'],
+
                         'featured' => false,
                     ],
                     [
                         'name' => 'CLASSIC',
                         'price' => '1200€',
-                        'title' => '1 collaborateur senior niveau intermédiaire :',
+                        'title' => '1 collaborateur niveau senior :',
                         'description' => [
-                            'Traitement et révision avec édition de rapport',
-                            'Bon niveau de français',
-                            'Travail autonome sans révision nécessaire',
+                            'Tenue comptable (saisie, révision, lettrage, rapprochement bancaire)',
+                            'Fiscalité (préparation de la TVA, situation/bilan, préparation de la liasse fiscale)',
                         ],
                         'featured' => false,
                     ],
@@ -38,17 +39,17 @@
                         'name' => 'STRATEGIC',
                         'price' => '1750€',
                         'title' => '1 binôme senior + junior :',
-                        'description' => ['Traitement et révision', 'Français excellent', 'Rapport d\'activité inclus'],
+                        'description' => ['Tenue comptable (saisie, révision, lettrage, rapprochement bancaire)', 'Fiscalité (préparation de la TVA, situation/bilan, préparation de la liasse fiscale)', 'Rapport d activité (listing pièces manquantes, points en suspens, différents contrôle dans un fichier Excel)'],
                         'featured' => false,
                     ],
                     [
                         'name' => 'MAGIC',
                         'price' => 'Sur devis',
                         'description' => [
-                            'Personnalisé selon vos besoins',
-                            'Équipe dédiée',
-                            'Support prioritaire',
-                            'Solutions sur mesure',
+                            'Personnalisé et sur mesure',
+                            // 'Équipe dédiée',
+                            // 'Support prioritaire',
+                            // 'Solutions sur mesure',
                         ],
                         'featured' => false,
                     ],
@@ -57,13 +58,14 @@
 
             @foreach ($tiers as $tier)
                 <div class="col-md-6 mb-4" data-aos="fade-up" data-aos-delay="{{ $loop->index * 100 }}">
-                    <div class="card h-100 border {{ $tier['featured'] ? 'border-warning' : '' }}" style="background: transparent">
+                    <div class="card h-100 border {{ $tier['featured'] ? 'border-warning' : '' }}"
+                        style="background: transparent">
                         <div class="card-body">
                             <h4 class="card-title ">{{ $tier['name'] }}</h4>
 
                             @if ($tier['featured'])
-                                <span class="badge bg-warning text-dark position-absolute"
-                                    style="top: 20px;">Best seller</span>
+                                <span class="badge bg-warning text-dark position-absolute" style="top: 20px;">Best
+                                    seller</span>
                             @endif
 
                             <p class="mt-3 display-4 fw-bold ">{{ $tier['price'] }}</p>
