88f2bb10435d40db29cc048389bef95f611827fe
CousemaAnjary <anjaryabdillah@gmail.com>
2024-10-23 21:29:58 +0300

    Refactoriser la mise en page de la barre de navigation

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-expand-lg navbar-dark bg-transparent position-absolute w-100">
+<nav class="navbar navbar-expand-lg navbar-dark bg-transparent">
     <div class="container">
         <!-- Logo -->
         <a class="navbar-brand" href="#">
@@ -6,27 +6,31 @@
         </a>
 
         <!-- Bouton pour le menu mobile -->
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
 
         <!-- Liens du menu -->
         <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav ms-auto  gap-4">
+            <ul class="navbar-nav ms-auto gap-4">
                 <li class="nav-item">
-                    <a class="nav-link text-white" href="#"><i class="fas fa-info-circle"></i> À propos de nous</a>
+                    <a class="nav-link text-white" href="#about-us-section"><i class="fas fa-info-circle"></i> À propos
+                        de nous</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link text-white" href="#"><i class="fas fa-tasks"></i> Nos missions</a>
+                    <a class="nav-link text-white" href="#mission-section"><i class="fas fa-tasks"></i> Nos missions</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link text-white" href="#"><i class="fas fa-gift"></i> Nos offres</a>
+                    <a class="nav-link text-white" href="#offre-section"><i class="fas fa-gift"></i> Nos offres</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link text-white" href="#"><i class="fas fa-hands-helping"></i> Nos valeurs</a>
+                    <a class="nav-link text-white" href="#valeur-section"><i class="fas fa-hands-helping"></i> Nos
+                        valeurs</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link text-white" href="#"><i class="fas fa-check-circle"></i> Nos avantages</a>
+                    <a class="nav-link text-white" href="#avantage-section"><i class="fas fa-check-circle"></i> Nos
+                        avantages</a>
                 </li>
             </ul>
         </div>
