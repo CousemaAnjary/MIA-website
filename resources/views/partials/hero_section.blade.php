2b851f50273ba8aa964b36c18bc293aea8afd1ec
CousemaAnjary <anjaryabdillah@gmail.com>
2024-10-23 23:26:28 +0300

    Refactoriser la mise en page de la barre de navigation et la hauteur de la section d'en-tête
    - Rendre la navigation collante
    - Ajouter du style au menu mobile
    - Modifier le style des liens dans le menu
    - Ajouter des espacements pour les petits écrans
    - Augmenter la taille du texte pour les écrans mobiles

--- resources/views/partials/hero_section.blade.php
+++ resources/views/partials/hero_section.blade.php
@@ -1,12 +1,11 @@
 <div class="container d-flex flex-column justify-content-center align-items-start h-75 text-start text-white" data-aos="fade-right">
     <h1 class="display-4 font-weight-bold mb-4" data-aos="zoom-in">
-        Nous déléguer une partie de vos activités, <br> c'est gagner du temps pour mieux accompagner vos précieux
-        clients
+        Nous déléguer une partie de vos activités, <br> c'est gagner du temps pour mieux accompagner vos précieux clients
     </h1>
     <p class="text-white fst-italic fs-5" data-aos="fade-up">
         Vous méritez l'excellence sans négliger la prudence. Laissez-nous vous aider avec toute notre compétence.
     </p>
-    <div class="mt-4 d-flex flex-column flex-md-row justify-content-start gap-3" data-aos="fade-up">
+    <div class="mt-4 d-flex flex-row flex-wrap justify-content-start gap-3" ">
         <a href="#offre-section" class="btn btn-lg text-white" style="background-color: #002c69">
             <i class="fas fa-gift"></i> Nos offres
         </a>
@@ -15,4 +14,3 @@
         </a>
     </div>
 </div>
-
