7b82c0febdde4cde1338149de8873f41a85ad681
CousemaAnjary <anjaryabdillah@gmail.com>
2024-10-23 20:50:21 +0300

    Refactoriser les routes pour utiliser le contrôleur PageController

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
+use App\Http\Controllers\PageController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('pages.home.index');
-});
+
+Route::get('/',[PageController::class, 'index']);
